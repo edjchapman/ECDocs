167d04db47425113862ca95ddad79ea82e8fa771
Ed Chapman <edchapman88@gmail.com>
2024-05-24 02:47:55 +0100

    Upgrade and add localisation.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.tsx',
 });
 
-module.exports = withNextra();
+module.exports = withNextra({
+    i18n: {
+        locales: ['en-GB', 'en-US'],
+        defaultLocale: 'en-GB'
+    }
+});
